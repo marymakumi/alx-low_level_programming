c983c709edfff6991959c99d30ebe6e36a3f9214
marymakumi <makumimarynjerii@gmail.com>
2022-09-09 22:34:05 -0700

    typesize

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the various type sizes
+ * return - 0 to exit corretly
+ */
+
+int main(void)
+
+{
+	("size of a char: 1 byte(s)\n");
+	("size of an int: 4 byte(s)\n");
+	("size of a long int: 4 bytes(s)\n");
+	("size of a long long int: 8 byte(s)\n");
+	("size of a float: 4 byte(s)\n");
+	return (0);
